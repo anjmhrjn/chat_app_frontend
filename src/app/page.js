e3972999374119281d5f94c4f512333f505563f6
anjmhrjn <anjmhrjn1@gmail.com>
2025-06-25 23:40:31 -0400

    username update

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from "react";
 
 import { MdAddCircle } from "react-icons/md";
 import { IoEnter } from "react-icons/io5";
+import { FaEdit } from "react-icons/fa";
 
 import ChangeUsername from "@/components/ChangeUsername";
 import { useSocket } from "@/context/SocketContext";
@@ -20,9 +21,8 @@ export default function Home() {
     socket.on("tokenIssued", handleTokenIssued);
 
     return () => {
-      // Remove the listener when component unmounts
-      socket.off("needUsername");
-      socket.off("tokenIssued");
+      socket.off("needUsername", handleNeedUsername);
+      socket.off("tokenIssued", handleTokenIssued);
     };
   }, [isSocketReady, socket]);
 
@@ -34,8 +34,7 @@ export default function Home() {
     socket.emit("setUsername", { username: value });
   };
 
-  const handleTokenIssued = (msg) => {
-    console.log(msg);
+  const handleTokenIssued = () => {
     setShowUsernamePrompt(false);
   };
 
@@ -55,7 +54,17 @@ export default function Home() {
     <div className="h-screen flex justify-center items-center">
       <div className="w-full md:w-1/3 h-screen md:h-1/4">
         <div className="p-4 backdrop-blur-xl rounded-2xl">
-          <p className="text-center font-extrabold text-4xl mb-3">Welcome!</p>
+          <p className="text-center font-extrabold text-4xl">Welcome!</p>
+          {userInfo?.username && (
+            <p
+              className="flex items-center justify-center font-semibold text-2xl mb-3 cursor-pointer text-gray-600"
+              onClick={() => setShowUsernamePrompt(true)}
+            >
+              <span className="mr-3">{userInfo?.username}</span>
+              <FaEdit />
+            </p>
+          )}
+
           <div className="px-3 mb-3 text-xl">
             <button
               className="bg-green-600 hover:bg-green-700 text-white font-semibold rounded-full p-2 w-full"
