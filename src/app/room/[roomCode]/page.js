4fefbcb9714f35f85805aa5e69ec91cce1bbed70
anjmhrjn <anjmhrjn1@gmail.com>
2025-06-29 22:52:25 -0400

    join room error preview

--- src/app/room/[roomCode]/page.js
+++ src/app/room/[roomCode]/page.js
@@ -8,12 +8,14 @@ import ChangeUsername from "@/components/ChangeUsername";
 import { usePreLoader } from "@/context/PreLoaderContext";
 import axios from "../../../axios";
 import { toast } from "react-toastify";
+import Image from "next/image";
 
 export default function Room() {
   const { roomCode } = useParams();
   const { socket, userInfo, isSocketReady } = useSocket();
   const [showUsernamePrompt, setShowUsernamePrompt] = useState(false);
   const [hasJoinedRoom, setHasJoinedRoom] = useState(false);
+  const [hasError, setHasError] = useState(false);
   const { setIsLoading } = usePreLoader();
   const hasAttemptedJoin = useRef(false);
 
@@ -35,7 +37,7 @@ export default function Room() {
   useEffect(() => {
     if (userInfo && !hasAttemptedJoin.current) {
       joinRoom();
-      hasAttemptedJoin.current = true
+      hasAttemptedJoin.current = true;
     }
   }, [userInfo]);
 
@@ -62,10 +64,13 @@ export default function Room() {
       const result = await axios.post(`/room/join-room`, { roomCode });
       if (result?.data?.success) {
         socket.emit("joinRoom", { roomCode: roomCode });
+      } else {
+        setHasError(true);
       }
     } catch (err) {
       console.log(err);
       toast.error("Could not join a room");
+      setHasError(true);
     } finally {
       setIsLoading(false);
     }
@@ -75,15 +80,31 @@ export default function Room() {
     <div>
       {showUsernamePrompt ? (
         <ChangeUsername updateUsername={updateUsername} userInfo={userInfo} />
+      ) : hasJoinedRoom ? (
+        <ChatRoom
+          roomCode={roomCode}
+          userInfo={userInfo}
+          socket={socket}
+          setIsLoading={setIsLoading}
+        />
       ) : (
-        hasJoinedRoom && (
-          <ChatRoom
-            roomCode={roomCode}
-            userInfo={userInfo}
-            socket={socket}
-            setIsLoading={setIsLoading}
-          />
-        )
+        <div className="w-full h-screen flex items-center justify-center">
+          <div className="w-2/3 flex justify-center">
+            <div>
+              <Image
+                src={`${hasError ? "/error.svg" : "/waiting.svg"}`}
+                alt="waiting"
+                height={500}
+                width={500}
+              />
+              <p className={`text-3xl font-extrabold`}>
+                {hasError
+                  ? "We could not process your request"
+                  : "Please wait while we process your request"}
+              </p>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
